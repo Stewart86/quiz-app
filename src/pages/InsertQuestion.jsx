e5b52bee5fbb66c1589d947f436f21fa8025e37a
Stewart Wong <stewart.wong@custodiotech.com.sg>
2021-01-21 22:23:59 +0800

    updated insert question

--- src/pages/InsertQuestion.jsx
+++ src/pages/InsertQuestion.jsx
@@ -1,34 +1,86 @@
+import { Button, Paper, Typography } from "@material-ui/core"
+import React, { useState } from "react"
+
 import { Editor } from "@tinymce/tinymce-react"
-import InsertMultipleChoice from "../components/InsertMultipleChoice";
-import React from "react"
-import { Typography } from "@material-ui/core"
+import InsertMultipleChoice from "../components/InsertMultipleChoice"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(3),
+  },
+}))
 
 export default function InsertQuestion() {
-   const handleEditorChange = (content, editor) => {
-     console.log('Content was updated:', content);
-   }
+  const classes = useStyles()
+
+  const [choices, setChoices] = useState([""])
+  const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState(0)
+
+  const handleRemoveClick = (index) => {
+    const list = [...choices]
+    list.splice(index, 1)
+    setChoices(list)
+  }
+
+  const handleAddClick = (event) => {
+    setChoices([...choices, event.value])
+  }
+  const handleEditorChange = (content, editor) => {
+    setQuestion(content)
+  }
+
+  const handleInsertQuestion = () => {
+    const answer = 1
+    console.log({
+      question: question,
+      choices: choices,
+      type: "multipleChoice",
+      answer: answer,
+    })
+    // db.collection("questions")
+    //   .add({
+    //     question: question,
+    //     choices: choices,
+    //     type: "multipleChoice",
+    //     answer: answer,
+    //   })
+    //   .then(() => console.log("done"))
+    //   .catch((error) => console.error(error))
+  }
   return (
     <>
-      <Typography variant={"h2"}>Insert Question</Typography>
-
-      <Editor
-        initialValue='<p>This is the initial content of the editor</p>'
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table paste code help wordcount",
-          ],
-          toolbar:
-            `undo redo | formatselect | bold italic backcolor | \
+      <Paper className={classes.paper}>
+        <Typography variant={"h2"}>Insert Question</Typography>
+        <Editor
+          initialValue='<p>This is the initial content of the editor</p>'
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image charmap print preview anchor",
+              "searchreplace visualblocks code fullscreen",
+              "insertdatetime media table paste code help wordcount",
+            ],
+            toolbar: `undo redo | formatselect | bold italic backcolor | \
              alignleft aligncenter alignright alignjustify | \
              bullist numlist outdent indent | removeformat | help`,
-        }}
-        onEditorChange={handleEditorChange}
-      />
-      <InsertMultipleChoice/>
+          }}
+          onEditorChange={handleEditorChange}
+        />
+        <InsertMultipleChoice
+          choices={choices}
+          handleRemoveClick={handleRemoveClick}
+          handleAddClick={handleAddClick}
+        />
+        <Button
+          variant='contained'
+          color={"primary"}
+          onClick={handleInsertQuestion}>
+          Insert
+        </Button>
+      </Paper>
     </>
   )
 }
