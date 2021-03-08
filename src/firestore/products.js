b5e7056f47c65288066f42e114d8ebcd873d5730
Stewart Wong <stewart.wong@custodiotech.com.sg>
2021-03-08 19:46:17 +0800

    Update products.js

--- src/firestore/products.js
+++ src/firestore/products.js
@@ -16,30 +16,31 @@ export const get = async () => {
     })
 }
 
-
 export const checkoutSession = async (currentUser) => {
-const docRef = await db
-  .collection('users')
-  .doc(currentUser.uid)
-  .collection('checkout_sessions')
-  .add({
-    price: 'price_1ISNO7AhlGA96d4eVM3dwyWP',
-    success_url: window.location.origin,
-    cancel_url: window.location.origin,
-  });
-// Wait for the CheckoutSession to get attached by the extension
-docRef.onSnapshot((snap) => {
-  const { error, sessionId } = snap.data();
-  if (error) {
-    // Show an error to your customer and 
-    // inspect your Cloud Function logs in the Firebase console.
-    alert(`An error occured: ${error.message}`);
-  }
-  if (sessionId) {
-    // We have a session, let's redirect to Checkout
-    // Init Stripe
-    const stripe = loadStripe('pk_test_51ISMnzAhlGA96d4e6jQvCCFReCKSAZsonqn7nammjSuMpqfteJQZiqoXFo2wmUKVXNXGtVDwmtQxq05lkiIRT5JW001zHApot2');
-    stripe.redirectToCheckout({ sessionId });
-  }
-});
+  const docRef = await db
+    .collection("users")
+    .doc(currentUser.uid)
+    .collection("checkout_sessions")
+    .add({
+      price: "price_1ISNO7AhlGA96d4eVM3dwyWP",
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+    })
+  // Wait for the CheckoutSession to get attached by the extension
+  docRef.onSnapshot((snap) => {
+    const { error, sessionId } = snap.data()
+    if (error) {
+      // Show an error to your customer and
+      // inspect your Cloud Function logs in the Firebase console.
+      alert(`An error occured: ${error.message}`)
+    }
+    if (sessionId)
+      // We have a session, let's redirect to Checkout
+      // Init Stripe
+      loadStripe(
+        "pk_test_51ISMnzAhlGA96d4e6jQvCCFReCKSAZsonqn7nammjSuMpqfteJQZiqoXFo2wmUKVXNXGtVDwmtQxq05lkiIRT5JW001zHApot2"
+      ).then((stripe) => {
+        stripe.redirectToCheckout({ sessionId })
+      })
+  })
 }
